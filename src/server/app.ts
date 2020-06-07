0fbd7f5b00e764ededd21fe7330bd7c36c9bec52
Stijn Piron <stijn.piron@gmail.com>
2020-06-07 20:55:18 +0200

    test client build folder path

--- src/server/app.ts
+++ src/server/app.ts
@@ -68,10 +68,10 @@ export class App {
 
   initializeClient(): void {
     if (process.env.NODE_ENV === 'production') {
-      this.app.use(express.static('client/build'));
+      this.app.use(express.static('src/client/build'));
 
       this.app.get('*', (_req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'src', 'client', 'build', 'index.html'));
       });
     }
   }
