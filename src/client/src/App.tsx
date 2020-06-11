d463b4b0c55e65980850af960017e1326dd51a8d
stijnpiron <stijn.piron@gmail.com>
2020-06-11 17:18:52 +0200

    cleaned up the react app

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return <h1>Hello World</h1>;
-};
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>My React app</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
